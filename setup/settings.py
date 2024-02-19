c1d4a881e2e3b9805ebd03774e1b7f31ab04de6e
Luis Sales <luissales2608@gmail.com>
2024-02-19 19:03:55 -0300

    OK

--- setup/settings.py
+++ setup/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+from dj_database_url import parse as db_url
 
+SECRET_KEY = config('SECRET_KEY')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(=_lrp(7(9a#+tobwfe)h38i_%m=e-h()b@bam2&$4a^rkdq!o'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=config("DEBUG", cast= bool, default=False)
 
 ALLOWED_HOSTS = []
 
@@ -75,10 +78,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config("DATABASE_URL", default=f'sqlite:///{BASE_DIR/"db.sqlite3"}', cast=db_url )
 }
 
 
